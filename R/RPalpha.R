1e08aef7c373da9fa18b71945247e9f41ccb8401
Jacob Bradley <cobrbradley@gmail.com>
2021-02-10 00:19:29 +0000

    trying to make RPAlpha examples work

--- R/RPalpha.R
+++ R/RPalpha.R
@@ -18,7 +18,6 @@
 #' Out <- RPParallel(XTrain = Train$x, YTrain = Train$y, XTest = Test$x, d = 2, B1 = 10, 
 #'                   B2 = 10, base = "LDA", projmethod = "Haar", estmethod = "training", cores = 1)
 #' alpha <- RPalpha(RP.out = Out, Y = Train$y, p1 = sum(Train$y == 1)/length(Train$y))
-#' alpha
 
 RPalpha <- function(RP.out, 
                     Y, 
