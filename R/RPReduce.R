b566df5dc000e8e82fab396fed8f26ee0a4295a7
Jacob Bradley <cobrbradley@gmail.com>
2021-02-17 04:34:00 +0000

    documentation for functions RPConcatenateA, RPReduceA and RPDecomposeA

--- R/RPReduce.R
+++ R/RPReduce.R
@@ -1,21 +1,45 @@
+#' Choose a set of B1 projections
+#'
+#' @description Creates B1*B2 projections, and for each block of B2 selects the best projection.
+#'
+#' @param XTrain An n by p matrix containing the training data feature vectors.
+#' @param YTrain A vector of length n of the classes (either 1 or 2) of the training data.
+#' @param d The lower dimension of the image space of the projections.
+#' @param B1 The number of blocks.
+#' @param B2 The block size.
+#' @param base The base classifier one of "knn","LDA","QDA" or "other".
+#' @param projmethod Either "Haar", "Gaussian" or "axis".
+#' @param estmethod Method for estimating the test errors to choose the projection: either training error "training", leave-one-out "loo", or sample split "samplesplit".
+#'
+#' @return A p by d*B1 matrix, where each row block of size p by d is one of the B1 selections.
+#' @export
+#'
+#' @examples
+#' set.seed(100)
+#' Train <- RPModel(1, 50, 100, 0.5)
+#' Choose <- RPConcatenateA(XTrain = Train$x, YTrain = Train$y, d = 3, B1 = 10, B2 = 5, base = "QDA", 
+#' projmethod = "Haar", estmethod = "loo")
+
 RPConcatenateA <- function(XTrain, 
                            YTrain,
                            d,
                            B1 = 100,
                            B2 = 10,
                            base = "LDA",
+                           projmethod = "Haar",
                            estmethod = "training") {
   
   p <- ncol(XTrain)
-  
   A_concat <- matrix(0, p, d * B1)
   
+  print("Choosing projections")
   pb = utils::txtProgressBar(min = 0, max = B1, initial = 0, style = 3) 
   
   
   for (b1 in 1:B1) {
     A_concat[, (b1-1)*d + 1:d] <- RPChooseA(XTrain = XTrain, YTrain = YTrain, d = d, B1 = B1, 
-                                            B2 = B2, base = base, estmethod = estmethod)
+                                            B2 = B2, base = base, projmethod = projmethod, 
+                                            estmethod = estmethod)
   
     utils::setTxtProgressBar(pb, b1)
     
@@ -25,25 +49,79 @@ RPConcatenateA <- function(XTrain,
   
 }
 
-RPDecompose <- function(XTrain,
+#' Produce singular value decomposition for average choice of projection
+#'
+#' @description For the concatenated matrix containing B1 selections of p by B2 sized projection matrices, 
+#' outputs the singular value decomposition.
+#' 
+#' @param XTrain An n by p matrix containing the training data feature vectors.
+#' @param YTrain A vector of length n of the classes (either 1 or 2) of the training data. 
+#' @param d The lower dimension of the image space of the projections.
+#' @param B1 The number of blocks.
+#' @param B2 The block size.
+#' @param base The base classifier one of "knn","LDA","QDA" or "other".
+#' @param projmethod Either "Haar", "Gaussian" or "axis".
+#' @param estmethod Method for estimating the test errors to choose the projection: either training error "training", leave-one-out "loo", or sample split "samplesplit".
+#'
+#'
+#' @return A list of three entries: eigv, u and v. These correspond to the eigenvalues and the two factor matrices of the singular value decomposition.
+#' @export
+#'
+#' @examples
+#' set.seed(100)
+#' Train <- RPModel(1, 50, 100, 0.5)
+#' Choose <- RPDecomposeA(XTrain = Train$x, YTrain = Train$y, d = 3, B1 = 10, B2 = 5, base = "QDA", 
+#' projmethod = "Haar", estmethod = "loo")
+
+RPDecomposeA <- function(XTrain,
                      YTrain,
                      d,
                      B1 = 100,
                      B2 = 10,
                      base = "LDA",
+                     projmethod = "Haar",
                      estmethod = "training") {
   
   A_concat <- RPConcatenateA(XTrain = XTrain, YTrain = YTrain, d = d, B1 = B1, B2 = B2, 
-                             base = base, estmethod = estmethod)
+                             base = base, projmethod = projmethod, estmethod = estmethod)
   
+  print("Decomposing matrix")
   average_projection <- (1/B1) * A_concat %*% t(A_concat)
   decomposition <- svd(average_projection)
+  names(decomposition) <- c("eigv", "u", "v")
   
   return(decomposition)
   
 }
 
-RPReduce <- function(XTrain,
+#' Produce low-dimensional representations of a dataset
+#' 
+#' @description Implements the full supervised dimensionality reduction workflow, by generating then selecting matrices, decomposing the resultant concatenated matrix and then projecting onto a given sized space.
+#'
+#' @param XTrain An n by p matrix containing the training data feature vectors.
+#' @param YTrain A vector of length n of the classes (either 1 or 2) of the training data. 
+#' @param XTest An n.test by p matrix containing the test data feature vectors.
+#' @param YTest A vector of length n.test of the classes (either 1 or 2) of the test data. 
+#' @param reduced_dim The dimensionality of reduced data to produce.
+#' @param d The lower dimension of the image space of the projections.
+#' @param B1 The number of blocks.
+#' @param B2 The size of each block.
+#' @param base The base classifier one of "knn","LDA","QDA" or "other".
+#' @param projmethod Either "Haar", "Gaussian" or "axis".
+#' @param estmethod Method for estimating the test errors to choose the projection: either training error "training", leave-one-out "loo", or sample split "samplesplit".
+#' @param decomposition (optional) the result of a call to RPDecomposeA().
+#'
+#' @return A list with two elements, XTest_r and YTest. XTest_r is an n.test by reduced_dim sized matrix containing reduced data representations. YTest is whatever has been provided.
+#' @export
+#'
+#' @examples
+#' set.seed(100)
+#' Train <- RPModel(1, 50, 100, 0.5)
+#' Test <- RPModel(1, 50, 100, 0.5)
+#' Rep <- RPReduceA(XTrain = Train$x, YTrain = Train$y, XTest = Test$x, reduced_dim = 2, d = 3, 
+#' B1 = 10, B2 = 5, base = "QDA", projmethod = "Haar", estmethod = "loo")
+
+RPReduceA <- function(XTrain,
          YTrain,
          XTest,
          YTest = NULL,
@@ -52,16 +130,19 @@ RPReduce <- function(XTrain,
          B1 = 100,
          B2 = 10,
          base = "LDA",
+         projmethod = "Haar",
          estmethod = "training",
          decomposition = NULL) {
 
   if (is.null(decomposition)) {
-    decomposition <- RPDecompose(XTrain = XTrain, YTrain = YTrain, d = d, B1 = B1, B2 = B2, 
-                                 base = base, estmethod = estmethod)
+    decomposition <- RPDecomposeA(XTrain = XTrain, YTrain = YTrain, d = d, B1 = B1, B2 = B2, 
+                                 base = base, projmethod = projmethod, estmethod = estmethod)
   }
   
+  print("Producing reductions")
+  reductions <- XTest %*% decomposition$v[, 1:reduced_dim, drop = FALSE] 
   
-  
+  return(list(XTest_r = reductions, YTest = YTest))
 }
 
 
